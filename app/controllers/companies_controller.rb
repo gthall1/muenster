3ecfc0f3e4e00bafb1ebd9e98d18b42eb5d3a291
Griff Hall <griffhall1@gmail.com>
2014-06-19 10:46:24 -0400

    create and new page implemented

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,30 @@
+class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def new
+    @new_company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to company_path(@company)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :location, :industry, :description)
+  end
+end
+
+
